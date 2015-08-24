013e89be1853987409b5aa89fbf7a142da0ec710
David Westerink <davidakachaos@gmail.com>
2015-08-24 13:45:22 +0200

    Allow the use of a app name
    this is for managing multiple apps in one folder. You can now pass the
    application name to the task. This in turn will be picked up by the
    heroku commands.

--- lib/dotenv-heroku/tasks.rb
+++ lib/dotenv-heroku/tasks.rb
@@ -10,19 +10,20 @@ namespace :config do
   end
 
   desc "push the .env file to heroku config"
-  task :push, [:env_file] => :executable do |t, args|
+  task :push, [:env_file, :appname] => :executable do |t, args|
     args.with_defaults(env_file: ".env")
 
-    File.readlines(args[:env_file]).map(&:strip).each do |value|
-      sh "heroku config:set #{value}"
-    end
+    # Heroku allows setting env vars in 1 go
+    value = File.readlines(args[:env_file]).map(&:strip).join(' ')
+    sh "heroku config:set #{value} #{appname ? "--app #{appname}" : nil}"
   end
 
   desc "pull the config from heroku and write to .env file"
-  task :pull, [:env_file] => :executable do |t, args|
+  task :pull, [:env_file, :appname] => :executable do |t, args|
     args.with_defaults(env_file: ".env")
+    args.with_defaults(appname: nil)
 
-    remote_config = `heroku config`
+    remote_config = `heroku config #{appname ? "--app #{appname}" : nil}`
     remote_config or fail "could not fetch remote config"
     remote_config = remote_config.split("\n")
     remote_config.shift # remove the header
